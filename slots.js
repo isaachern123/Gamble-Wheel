818a73d488c6a277dc9d63874e32bd4d6c86c868
isaachern123 <isaac.a.hernandez12@gmail.com>
2023-08-16 21:58:01 -0500

    new

--- slots.js
+++ slots.js
@@ -8,6 +8,27 @@
 
 const prompt = require("prompt-sync")();
 
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+
 const deposit = () => {
     while (true) {
 
@@ -44,7 +65,7 @@ const getBet = (balance, lines) => {
         const bet = prompt("Enter the bet per line: ");
         const numberBet = parseFloat(bet);
 
-        if (isNaN(numberBet) || numberBet <= 0 || numberBet > (balance / lines) ){
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > (balance / lines)) {
             console.log("Invalid bet amount, try again.")
         } else {
             return numberOfLines;
@@ -53,7 +74,30 @@ const getBet = (balance, lines) => {
 }
 
 
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
 
+    const reels = [[], [], []];
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+
+    return reels;
+};
+
+const reels = spin();
+console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
