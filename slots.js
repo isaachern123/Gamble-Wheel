0142ac247e6653af18cb11f506f11ec054430827
isaachern123 <isaac.a.hernandez12@gmail.com>
2023-08-18 08:28:04 -0500

    stil working

--- slots.js
+++ slots.js
@@ -82,8 +82,9 @@ const spin = () => {
         }
     }
 
-    const reels = [[], [], []];
+    const reels = [];
     for (let i = 0; i < COLS; i++) {
+        reels.push([]);
         const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++) {
             const randomIndex = Math.floor(Math.random() * reelSymbols.length);
@@ -96,12 +97,31 @@ const spin = () => {
     return reels;
 };
 
-const reels = spin();
-console.log(reels);
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for(let j = 0; j <COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+
+    return rows;
+}
+
+
+
+
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
-
+const reels = spin();
+const rows = transpose(reels);
+console.log(reels);
+console.log(rows)
 
 
 
